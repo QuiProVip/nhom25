9a036c6a0948af403187e65f82bbeae3cfa49d20
triphuc186 <117024721+triphuc186@users.noreply.github.com>
2022-11-03 22:32:29 +0700

    Update

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
-
+#include "mylib.h"
+#include <iostream>
+#include <string>
+#define sl_ground 90
+#include<fstream> // doc ghi file
+#include<windows.h> // xoa man hinh
+using namespace std;
+int h = 5;//hight
+int xdino = 25; int ydino = 17;
+int xdat = 10; int ydat = ydino + h;
+char ground[3][sl_ground + 1];
+int jump = 14; bool kt_jump = false;
+int hiscore;
+string dino[6] =
+{
+        "           e-e ",
+        "         /(\\_/)",
+        ".___.---' /`-' ",
+        " `-._.  )/,,   ",
+        "     //        ",//4
+        "    ^^         ",//5
+};
+string die = "           x-x ";
+string space = "               ";
+string tree[5]
+{
+        "+++",
+        "+++",
+        "+++",
+        " + ",
+        " + ",
+};
